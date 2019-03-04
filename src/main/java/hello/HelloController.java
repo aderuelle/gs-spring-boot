34acbb77cb082b9035b650de3d91e4613b25a5c5
Alexis Deruelle <alexis.deruelle@gmail.com>
2019-03-04 07:32:49 +0100

    Tomcatization

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,26 @@
+package hello;
+
+import java.util.Collection;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/hello")
+    public Collection<String> sayHello() {
+        return IntStream.range(0, 10)
+          .mapToObj(i -> "Hello number " + i)
+          .collect(Collectors.toList());
+    }
+
+}
